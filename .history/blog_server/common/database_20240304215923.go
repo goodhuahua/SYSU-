d234ce44a036671769ed2dcc16bc65ebcf515910
goodhuahua <115097503+goodhuahua@users.noreply.github.com>
2024-03-04 22:04:04 +0800

    test

---
+++ .history/blog_server/common/database_20240304215923.go
@@ -0,0 +1,45 @@
+package common
+
+import (
+	"blog_server/model"
+	"fmt"
+	"net/url"
+
+	"github.com/jinzhu/gorm"
+)
+
+var DB *gorm.DB
+
+// 好不好
+func InitDB() *gorm.DB {
+	driverName := "mysql"
+	user := "root"
+	password := "123456"
+	host := "localhost"
+	port := "3306"
+	database := "blog"
+	charset := "utf8"
+	loc := "Asia/Shanghai"
+	args := fmt.Sprintf("%s:%s@(%s:%s)/%s?charset=%s&parseTime=true&loc=%s",
+		user,
+		password,
+		host,
+		port,
+		database,
+		charset,
+		url.QueryEscape(loc))
+	// 连接数据库
+	db, err := gorm.Open(driverName, args)
+	if err != nil {
+		panic("failed to open databasen is : " + err.Error())
+	}
+	// 迁移数据表
+	db.AutoMigrate(&model.User{}, &model.Category{}, &model.Admin{})
+	db.AutoMigrate(&model.EmailInfo{})
+	DB = db
+	return db
+}
+
+func GetDB() *gorm.DB {
+	return DB
+}
